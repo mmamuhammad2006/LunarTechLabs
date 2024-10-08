0bb3afaaa19e04284faf53a2f25e612de2c18eac
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-08 21:01:52 +0500

    use EJS layouts and create contactus, aboutus page

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(expressLayouts);
+app.set('layout', './layouts/app');
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '../views'));
 app.get('/',(req,res)=>{
-    res.render('index');
-    // res.render('../index');
-    console.log('index')
+    res.render('pages/home');
+});
+app.get('/services',(req,res)=>{
+    res.render('services');
+});
+app.get('/contact-us',(req,res)=>{
+    res.render('pages/contactUS');
+});
+app.get('/about-us',(req,res)=>{
+    res.render('pages/aboutUs');
 });
 app.listen(5000);
