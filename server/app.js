1fd21b8d5e4c82e03cca8fcf2d4cc3b74675eb37
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-10 13:42:42 +0500

    implement google analytics

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 var bodyParser = require('body-parser');    
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '../public')));
@@ -12,6 +13,11 @@ app.set('layout', './layouts/app');
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '../views'));
 
+// google analytics
+app.use((req,res,next)=>{
+    res.locals.analyticsId = process.env.GOOGLE_ANALYTICS_ID;
+    next();
+})
 // Navbar links routes
 const navRoutes = require('./routes/navRoute');
 app.use('/',navRoutes);
@@ -19,9 +25,10 @@ app.use('/',navRoutes);
 // whatsapp route
 const whatsappRoute = require('./routes/whatsappRoute');
 app.use('/',whatsappRoute);
-// Send Mail route
 
+// Send Mail route
 const sendMail = require('./controller/sendMailController');
 app.use('/',sendMail);
+
 
 app.listen(5000);
