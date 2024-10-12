f714920a6f6de5db7a0cff4f88fd88e826a2854f
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-12 09:33:40 +0500

    implement caching using cache-express

--- server/routes/navRoute.js
+++ server/routes/navRoute.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const cacheExpress = require('cache-express');
 const router = express.Router();
 
-
+const cache = cacheExpress({
+    duration: 3600000,
+    cacheControl: true,
+    maxAge: 3600
+});
+router.use(cache);
 router.get('/',(req,res)=>{
-    res.render('pages/home',{title: 'Home'});
+    try{
+        res.render('pages/home',{title: 'Home'});
+    }catch(error){
+        res.status(500).send('Internal Server Error')
+    }
 });
 router.get('/services',(req,res)=>{
-    res.render('pages/services',{title: 'Products'});
+    try{
+        res.render('pages/services',{title: 'Products'});
+    }catch(error){
+        res.status(500).send('Internal Server Error')
+    }
 });
 router.get('/contact-us',(req,res)=>{
-    res.render('pages/contactUS',{title: 'Contact Us'});
+    try{
+        res.render('pages/contactUS',{title: 'Contact Us'});
+    }catch (error){
+        res.status(500).send('Internal Server Error')
+    }
 });
 router.get('/about-us',(req,res)=>{
-    res.render('pages/aboutUs',{title: 'About Us'});
+    try{
+        res.render('pages/aboutUs',{title: 'About Us'});
+    }catch (error){
+        res.status(500).send('Internal Server Error')
+    }
 });
-
 module.exports = router;
--- server/routes/whatsappRoute.js
+++ server/routes/whatsappRoute.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // implemeting chat whatsapp logic
-router.get('/whatsapp',(req,res)=>{
-    const number = '923456047058';
-    const whatsappUrl = `https://wa.me/${number}`;
-    res.redirect(whatsappUrl);
+router.get('/whatsapp',async(req,res)=>{
+    try{
+        const number = '923456047058';
+        const whatsappUrl = `https://wa.me/${number}`;
+        res.redirect(whatsappUrl);
+    }catch(error){
+        console.error("Error in redirecting to WhatsApp:", error);
+        res.status(500).send('Internal Server Error');
+    }
+
 })
 
 module.exports = router
