77e1735a5580d856108e5c37767739817b1c2102
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-20 00:11:05 +0500

    debugging vercel deployment

--- server/api/index.js
+++ server/api/index.js
@@ -9,12 +9,12 @@ require('dotenv').config();
 helmetMiddleware(app);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 app.use(expressLayouts);
 
+app.set('views', path.join(__dirname, '../../views'));
 app.set('layout', './layouts/app');
 app.set('view engine','ejs');
-app.set('views', path.join(__dirname, '../views'));
 
 // google analytics
 app.use((req,res,next)=>{
