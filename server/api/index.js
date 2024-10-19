4287c15be40a3741eaa030a3d798ebf6ab548cce
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-19 23:36:53 +0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const helmet = require('./security/helmet');
 
 const app = express();
 
 // Middleware
-app.use(helmet());
+helmet(app);  // Helmet for security headers
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// EJS and Express Layouts
+// Static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Set EJS for templating
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-app.use(expressLayouts);
-app.set('layout', './layouts/app');
 
 // Routes
 const navRoutes = require('./routes/navRoute');
 const whatsappRoute = require('./routes/whatsappRoute');
-const sendMailController = require('./controllers/sendMailController');
+const sendMailController = require('./controller/sendMailController');
 
 app.use('/', navRoutes);
 app.use('/', whatsappRoute);
 app.use('/', sendMailController);
 
-// Export the Express app as a serverless function
+// Export the app as a module
 module.exports = app;
